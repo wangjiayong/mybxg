d467eda9f99646452b496ba167dae3fbd655c598
wjy <1053826935@qq.com>
2017-09-25 12:46:37 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,21 +1,40 @@
-define(['jquery','template','util'],function($,template,util){
-    var tcid=util.qs('tc_id')
-   if (tcid){
-    //如果点击了编辑才会有tcid编辑讲师
-       $.ajax({
-           type:'get',
-           url:'/api/teacher/edit',
-           data:{tc_id:tcid},
-           dataType:'json',
-           success:function(data){
-               data.result.op='编辑讲师'//给对象自定义一个属性
-              var html=template('teacherTpl',data.result)
-               $('#teacherInfo').html(html)
-           }
-       })
-   }else {
-       //编辑讲师和添加讲师共享一个页面
-       var html=template('teacherTpl',{op:'添加讲师'})//传进去一个自定义对象{op:'添加讲师'}，在模板中直接把op取出来
-       $('#teacherInfo').html(html)
-   }
+define(['jquery', 'template', 'util'], function ($, template, util) {
+    var tcid = util.qs('tc_id')
+    if (tcid) {
+        //如果点击了编辑才会有tcid编辑讲师
+        $.ajax({
+            type: 'get',
+            url: '/api/teacher/edit',
+            data: {tc_id: tcid},
+            dataType: 'json',
+            success: function (data) {
+                data.result.op = '编辑讲师'//给对象自定义一个属性
+                var html = template('teacherTpl', data.result)
+                $('#teacherInfo').html(html)
+                submitForm('/api/teacher/update')
+            }
+        })
+    } else {
+        //编辑讲师和添加讲师共享一个页面
+        var html = template('teacherTpl', {op: '添加讲师'})//传进去一个自定义对象{op:'添加讲师'}，在模板中直接把op取出来
+        $('#teacherInfo').html(html)
+        submitForm('/api/teacher/add')
+
+    }
+    function submitForm(url) {
+        $('#teacherBtn').click(function () {
+            $.ajax({
+                type: 'post',
+                url: url,
+                data: $('#teacherform').serialize(),//这里是把页面上做的操作转化成数据提交给后台来处理，把数据传给后台会进行对数据的添加和修改
+                dataType: 'json',
+                success: function (data) {
+                    if (data.code == 200) {
+                       location.href='/teacher/list'
+                    }
+                }
+
+            })
+        })
+    }
 })
