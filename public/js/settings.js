c08416d58578f318b43db39d56cbfea500e37c3f
wjy <1053826935@qq.com>
2017-09-26 21:53:03 +0800

    完成头像上传

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,12 +1,27 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','uploadify'],function($,template){
     $.ajax({
         type:'get',
         url:'/api/teacher/profile',
         dataType:'json',
         success:function(data){
-            console.log(data);
             var html=template('settingsTpl',data.result)
             $('#settingsInfo').html(html)
+            $('#upfile').uploadify({
+                swf : '/public/assets/uploadify/uploadify.swf',
+                uploader : '/api/uploader/avatar',
+                fileObjName : 'tc_avatar',
+                width : 120,
+                height : 120,
+                buttonText : '',
+                itemTemplate:'<span></span>',
+                onUploadSuccess : function(file,data){
+                    data = JSON.parse(data);
+                    if(data.code == 200){
+                        console.log(data.result.path);
+                        $('.preview img').attr('src',data.result.path);
+                    }
+                }
+            });
         }
     })
 })
