cddacdf91ba5f8fa6297ac744496ae350d0550bc
wjy <1053826935@qq.com>
2017-09-27 21:12:07 +0800

    添加富文本功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','uploadify','region'],function($,template){
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
     $.ajax({
         type:'get',
         url:'/api/teacher/profile',
@@ -26,6 +26,13 @@ define(['jquery','template','uploadify','region'],function($,template){
             //省市县三级联动
             $('#pcd').region({
                 url:'/public/assets/jquery-region/region.json'
+            })
+            //处理富文本
+            CKEDITOR.replace('editor',{
+                toolbarGroups : [
+                { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+            ]
             })
         }
     })
