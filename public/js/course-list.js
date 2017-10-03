7d18b3a466616e1f84eb1b10d15f9627ec159d3a
wjy <1053826935@qq.com>
2017-10-03 13:47:21 +0800

    完成课程列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,3 +1,12 @@
 define(['jquery','template','util'],function($,template,util){
 util.setMenu(location.pathname)
+    $.ajax({
+        type:'get',
+        url:'/api/course',
+        dateType:'json',
+        success:function(data){
+           var html=template('courseTpl',data)
+            $('#courseInfo').html(html)
+        }
+    })
 })
