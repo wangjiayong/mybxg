8a951f755bd328266b3a4021f00b99bfedec64e2
wjy <1053826935@qq.com>
2017-10-03 17:30:53 +0800

    完成二级分类联动操作

--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util'], function ($, template, util) {
+define(['jquery', 'template', 'util','ckeditor'], function ($, template, util,CKEDITOR) {
     util.setMenu('/course/add')//不管是从哪个页面跳转过来的都让它选中课程添加
     var csid = util.qs('cs_id')//本页面的数据请求需要其他页面传过来的id（获取地址栏里面的id）
     var flag = util.qs('flag')//自定义一个flag，来判断跳转到当前页面是需要进行编辑还是添加操作
@@ -16,6 +16,23 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
          }
             var html=template('basicTpl',data.result)
             $('#basicInfo').html(html)
+            //一二级菜单关联，使用后台接口的方式
+            $('#firstType').change(function(){
+                var pid=$(this).val()
+                $.ajax({
+                    type:'get',
+                    url:'/api/category/child',//通过后台的方式来实现下拉菜单的关联关系
+                    data:{cg_id:pid},//把获取的id发送给后台
+                    datatype:'json',
+                    success:function(data){
+                       var tpl=' <option value="">请选择二级分类</option>{{each result}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}'
+                        var html=template.render(tpl,data)
+                        $('#secondType').html(html)
+                    }
+                })
+            })
+            //该页面的富文本部分
+            CKEDITOR.replace('editor')//自定义可以参考settings.js里面
         }
     })
 })
